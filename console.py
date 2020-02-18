f75a8f14dc112fa7294989d531804e4aa88bca75
iheb1919 <ihebmejri14@gmail.com>
2020-02-18 11:58:29 +0100

    ff

--- console.py
+++ console.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 import cmd
 from models.base_model import BaseModel
+
+
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
+
     def do_quit(self, line):
         """
         Quit command to exit the program
         """
         return True
+
     def do_EOF(self, line):
         """
         Quit command to exit the program
         """
         print()
         return True
+
     def emptyline(self):
         pass
-        
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
