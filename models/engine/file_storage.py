a0404bf806609945917ef47d43e82bf74be804b9
iheb1919 <ihebmejri14@gmail.com>
2020-02-18 23:24:44 +0100

    ff

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-'''
-FileStorage - file storage
-'''
-
+"""
+Define: FileStorage Class
+"""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -11,40 +10,42 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-import os.path
 
 
-class FileStorage(BaseModel):
-    '''FileStorage - file storage'''
-    def __init__(self):
-        '''FileStorage - file storage'''
-        self.__file_path = 'file.json'
-        self.__objects = {}
+class FileStorage:
+    """Represent an File Storage.
+    """
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        '''all - return all objects'''
-        return (self.__objects)
+        """Retreive all class instances objects
+        """
+        return FileStorage.__objects
 
     def new(self, obj):
-        '''new - create new object'''
-        id1 = getattr(obj, 'id')
-        self.__objects[obj.to_dict()['__class__'] + '.' + id1] = obj
+        """Create new Object
+        """
+        FileStorage.__objects["{}.{}".format(
+            obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
-        '''save - save changes in object or new'''
-        dict2 = {}
-        for key, value in self.__objects.items():
-            dict2[key] = value.to_dict()
-            with open(self.__file_path, mode='w', encoding="utf-8") as f:
-                f.write(json.dumps(dict2))
+        """Save new model
+        """
+        to_store = {obj: FileStorage.__objects[obj].to_dict(
+        ) for obj in FileStorage.__objects.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(to_store, f, indent=2)
 
     def reload(self):
-        '''reload - reload objects or new'''
-        import os.path
-        if os.path.isfile(self.__file_path):
-            with open(self.__file_path, encoding="utf-8", mode='r') as f:
-                json_string = f.read()
-                if json_string:
-                    dict1 = json.loads(json_string)
-                    for key, value in dict1.items():
-                        self.__objects[key] = eval(value['__class__'])(**value)
+        """Reload data from json file
+        """
+        try:
+            with open(FileStorage.__file_path) as f:
+                data = json.load(f)
+                for i in data.values():
+                    cl_name = i["__class__"]
+                    del i["__class__"]
+                    self.new(eval(cl_name)(**i))
+        except Exception:
+            pass
