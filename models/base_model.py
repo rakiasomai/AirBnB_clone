ba150612b50c23280c3ff767b35c6f2969990ce4
iheb1919 <ihebmejri14@gmail.com>
2020-02-18 11:25:08 +0100

    ff

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 import uuid
-from datetime import datetime, date, time
+import datetime
+import json
 
 
 class BaseModel:
@@ -12,30 +13,44 @@ class BaseModel:
         """
         __init__
         """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key is not '__class__':
+                    setattr(self, key, value)
+            self.__class__.__name__ = kwargs['__class__']
+            self.created_at = datetime.datetime.strptime(kwargs['created_at'],
+                                                         time_format)
+            self.updated_at = datetime.datetime.strptime(kwargs['updated_at'],
+                                                         time_format)
+        else:
+            """storage.new(self)"""
 
     def __str__(self):
         """
         print: [<class name>] (<self.id>) <self.__dict__>
         """
-        return("[<{}>] (<{}>) <{}>".format(self.__class__.__name__,
-                                           self.id, self.__dict__))
+        return("[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__))
 
     def save(self):
             """
             update the update_at with the current datetime
             """
-            self.updated_at = datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
             """
             returns a dictionary containing all keys/values
             of __dict__ of the instance
             """
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            dictt = self.__dict__ 
+            dictt = {}
+            for key, value in self.__dict__.items():
+                dictt[key] = value
+            dictt['created_at'] = self.created_at.isoformat()
+            dictt['updated_at'] = self.updated_at.isoformat()
             dictt['__class__'] = self.__class__.__name__
-            return self.__dict__
+            return dictt
