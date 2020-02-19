8b5f6c7141af73cc54b14750aa39f76b027f2277
rakiasomai <1069@holbertonschool.com>
2020-02-19 15:46:24 +0000

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-Define: FileStorage Class
-"""
+'''
+Def: Class storage
+'''
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -13,38 +13,33 @@ from models.review import Review
 
 
 class FileStorage:
-    """Represent an File Storage.
-    """
+    '''Representing file Storage.
+    '''
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Retreive all class instances objects
-        """
+        '''Retreive all class instances objects
+        '''
         return FileStorage.__objects
 
     def new(self, obj):
-        """Create new Object
-        """
+        '''Creating a new Object
+        '''
         FileStorage.__objects["{}.{}".format(
             obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
-        """Save new model
-        
+        '''def save
+        '''
         to_store = {obj: FileStorage.__objects[obj].to_dict(
         ) for obj in FileStorage.__objects.keys()}
-        with open(FileStorage.__file_path, "a", encoding="utf-8") as f:
-            json.dump(to_store, f, indent=2)"""
-        d = {}
-        for key, value in self.__objects.items():
-            d[key] = value.to_dict()
-            with open(self.__file_path, mode='w', encoding="utf-8") as f:
-                f.write(json.dumps(d))
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(to_store, f, indent=2)
 
     def reload(self):
-        """Reload data from json file
-        """
+        '''Reload data
+        '''
         try:
             with open(FileStorage.__file_path, "r") as f:
                 data = json.load(f)
